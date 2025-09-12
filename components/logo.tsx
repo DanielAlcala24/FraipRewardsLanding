bdbe11fc48fd4242d98572a91201d76c7cc3457d
Daniel Alcalá <danielav24102001@gmail.com>
2025-09-12 07:54:57 -0600

    first commit

---
+++ components/logo.tsx
@@ -0,0 +1,29 @@
+import { cn } from '@/lib/utils'
+import { motion } from 'motion/react'
+import Image from "next/image";
+
+
+export const Logo = ({ className }: { className?: string }) => {
+    return (
+        <Image
+            src="/logo.png"
+            alt="Logo"
+            width={38}   // equivalente a size-7 (7 * 4px = 28px)
+            height={38}
+            className={cn("size-10 w-10", className)}
+        />
+
+    )
+}
+
+export const LogoStroke = ({ className }: { className?: string }) => {
+    return (
+        <Image
+            src="/logo.png"
+            alt="Logo"
+            width={38}   // equivalente a size-7 (7 * 4px = 28px)
+            height={38}
+            className={cn("size-10 w-10", className)}
+        />
+    )
+}
